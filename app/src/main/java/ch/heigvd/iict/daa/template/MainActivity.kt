9d3b306d7f5437dd90ed4f498c5a0e413592dd13
emilie-h <emilie.bressoud@heig-vd.ch>
2024-10-03 18:48:28 +0200

    chore: fix, cleanup unused layout

--- app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
@@ -2,7 +2,6 @@ package ch.heigvd.iict.daa.template
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_relative)
 
-        val btn = findViewById<Button>(R.id.connect)
-        btn.setOnClickListener { // Displaying simple Toast message
-            Toast.makeText(applicationContext, "This a toast message", Toast.LENGTH_LONG).show()
-        }
+        //val btn = findViewById<Button>(R.id.edit)
+
     }
 }

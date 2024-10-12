ddb8b4dd76498d2819c4a763b777be6c05b78e00
emilie-h <emilie.bressoud@heig-vd.ch>
2024-10-12 10:05:24 +0200

    chore: cleanup unused function

--- app/src/main/java/ch/heigvd/iict/daa/template/fragment/StepFragment.kt
+++ app/src/main/java/ch/heigvd/iict/daa/template/fragment/StepFragment.kt
@@ -14,10 +14,6 @@ class StepFragment : Fragment() {
 
     private var step: Int = 0
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
